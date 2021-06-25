ddd1af575b6394013243aeeb865e135e5d70d504
a-n-a-s <mirzaanas815@gmail.com>
2021-06-25 15:30:13 +0530

    done

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const newCard = (taskData) => `
 <div class="card-body border border-dark rounded">
   <h5 class="card-title">${taskData.taskTitle}</h5>
   <p class="card-text">${taskData.taskDesc}</p>
-  <a href="#" class="btn btn-danger">Delete Task</a>
+  <a href="#" class="btn btn-danger" onclick="deleteTask()">Delete Task</a>
 </div>
 </div>
 `;
-const globalStorage = [];
+let globalStorage = [];
+const loadData = () => {
+  const getCarddata = localStorage.getItem("todo");
+  const {cards} = JSON.parse(getCarddata);
+  console.log(cards);
+
+  cards.map((cardObject) => {
+    container.insertAdjacentHTML("beforeend" , newCard(cardObject));
+    globalStorage.push(cardObject);
+  });
+}
 const addTask = () => {
   taskData = {
     id: Date.now(),
@@ -20,10 +30,24 @@ const addTask = () => {
 
   globalStorage.push(taskData);
 
-  localStorage.setItem("todo",JSON.stringify(taskData));
+  localStorage.setItem("todo",JSON.stringify({cards : globalStorage}));
   
 };
 
 const deleteTask = () => {
+  event = window.event;
+  const targetId = event.target.id;
+  const tagName = event.target.tagName;
+  
+  globalStorage = globalStorage.filter( (cardObject) => cardObject.id != targetId
+  );
+  
+  localStorage.setItem("todo",JSON.stringify(
+  newCard({cards : globalStorage})));
+  
+  
+  if(tagName == "A"){
+    return container.removeChild(event.target.parentNode.parentNode);
+  }
   
 };
